da3f1c8f24febb5f5ca8f28942804685ff3fc89a
syscurso <54544590+syscurso@users.noreply.github.com>
2022-12-27 13:45:52 +0100

    Update telegramBot.py

--- telegramBot.py
+++ telegramBot.py
@@ -3,24 +3,28 @@
 #Telegram t.me/syscursos
 
 from telethon import TelegramClient, events
-import apiData
 from datetime import datetime
 import pyjokes
+
+
+api_id = 15209149
+api_hash = '7016b258a72b41bd7047d7cb8f4ac3f7'
+chatName = '@syscursos'
 
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 joke = pyjokes.get_joke(language='es', category='all')
 
 
-client = TelegramClient('session', apiData.api_id, apiData.api_hash)
+client = TelegramClient('session', api_id, api_hash)
 
-@client.on(events.NewMessage(chats=apiData.chatName))
+@client.on(events.NewMessage(chats=chatName))
 
 async def my_event_handler(event):
 
   respuesta = answer(event.text)
   print(event.text)
-  await client.send_message(apiData.chatName, respuesta)
+  await client.send_message(chatName, respuesta)
 
 def answer(word):
       
