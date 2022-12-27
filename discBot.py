d28d3b08a732b5ad6dda80cc07491660bedec938
syscurso <54544590+syscurso@users.noreply.github.com>
2022-12-27 13:21:40 +0100

    Update discBot.py

--- discBot.py
+++ discBot.py
@@ -1,22 +1,3 @@
 
 
-from discord_easy_commands import EasyBot
-import pyjokes
-import discord
-
-
-
-
-token = 'MTA1NTgzMTAzMDkzMTY1MjYwOA.G_NZIx.IuGvMyRear8LqeqNm2IfPkSJqKmkkoO2GvGy50'
-
-
-joke = pyjokes.get_jokes(language='es', category='all')
-intents = discord.Intents.all()
-bot = EasyBot(intents = intents)
-
-
-bot.setCommand('!youtube', 'www.youtube.com')
-bot.setCommand('!instagram','syscursos')
-bot.setCommand('!chiste', joke)
-
-bot.run(token)
+print('hola')
