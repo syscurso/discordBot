4f42711be482acae2c857c0e2ccab5187d072098
syscurso <54544590+syscurso@users.noreply.github.com>
2022-12-27 13:38:21 +0100

    Update and rename discBot.py to telegramBot.py

---
+++ telegramBot.py
@@ -0,0 +1,22 @@
+
+
+from discord_easy_commands import EasyBot
+import pyjokes
+import discord
+
+
+
+
+token = 'MTA1NzI0NTk4ODg2MDk4NTM2NQ.Ghh62y.kccHOicG-49JhKfQODLoNtnGRoEaEyeolIZ46M'
+
+
+joke = pyjokes.get_jokes(language='es', category='all')
+intents = discord.Intents.all()
+bot = EasyBot(intents = intents)
+
+
+bot.setCommand('!youtube', 'www.youtube.com')
+bot.setCommand('!instagram','syscursos')
+bot.setCommand('!chiste', joke)
+
+bot.run(token)
